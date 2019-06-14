2aa98d26af3edddde9535c979ad4724d933e9cc8
amateu-dev <dipesh.sukhani@gmail.com>
2019-06-15 00:40:05 +0800

    Working PetShop SmartContract myVersion

--- truffle_pet_shop_dkversion.sol
+++ truffle_pet_shop_dkversion.sol
@@ -1,30 +1,35 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
+    struct Pet{
+        string name; 
+        uint8 adoptionStatus;
+        address owner;
+    } // this is the struct class of the pets to be used when a pet is created or deposited 
+    
     address[] public adopters; // this is the array of all the adopters
     
-    struct Pet{
-        uint adoptionStatus;
-        string name; 
-    } // this is the struct class of the pets to be used when a pet is created or deposited 
-
-    mapping (address => Pet) pet_address; // this is the mapping of the pets to their struct instances
+    mapping (string => Pet) public pets; // this is the mapping of the pets to their struct instances
     
-    address[] public pets; // this is array of all the pets
+    // Pet[] public pets; // this is array of all the pets
     
         
     function create_pet(string memory _petName) public {
-        // this is to create a new pet using the struct pet above
-        pets.push(msg.sender); // this will push the address of the pet in the pets array
-        pet_address[]
+        Pet memory newPet = Pet({
+            name: _petName,
+            adoptionStatus: 1,
+            owner: address(0)
+        });
         
-        
+        pets[_petName] = newPet;
         
     }
     
-    function adopt(uint8 _petId) public {
-        // ideally this function should be payable, that is to say that the person who would like to adopt the pet must pay or make a little contribution
+    function adopt(string memory _petName) public payable {
+        require(pets[_petName].adoptionStatus == 1);
         adopters.push(msg.sender);
+        pets[_petName].owner = msg.sender;
+        pets[_petName].adoptionStatus = 0;
     }
     
 }
